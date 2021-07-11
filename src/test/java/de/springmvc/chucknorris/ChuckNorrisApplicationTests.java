70de671beceea10cebdecc90fceaecd7567c12f8
Britta Lange <britta.lange@mailbox.org>
2021-07-11 07:40:40 +0200

    Initial commit.

---
+++ src/test/java/de/springmvc/chucknorris/ChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package de.springmvc.chucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
