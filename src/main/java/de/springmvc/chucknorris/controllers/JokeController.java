a764cf9dcfae5e29ccc7fa2b1b2f5f99d44c2c91
Britta Lange <britta.lange@mailbox.org>
2021-08-29 19:27:46 +0200

    Added Bootstrap CSS and jQuery webjars.

--- src/main/java/de/springmvc/chucknorris/controllers/JokeController.java
+++ src/main/java/de/springmvc/chucknorris/controllers/JokeController.java
@@ -8,14 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokeController {
 
-    JokeService jokeService;
+    private final JokeService jokeService;
+
 
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
     @RequestMapping({"/", ""})
-    public String getChuckNorrisJokes(Model model) { // Here comes the model from M(odel)VC.
+    public String getChuckNorrisJokes(Model model) { // Here comes the Spring model from M(odel)VC.
 
         // Here gets the Thymeleaf template access to "jokes". The model gets a property/attribute. It is implemented as a kind of hash map. jokes is the key, getJoke() returns the value.
         model.addAttribute("jokes", jokeService.getJoke());
